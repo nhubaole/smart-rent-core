6f18c5446fe9d436a429682a0a397635aa519e58
nhubaole <okamisameko7123@gmail.com>
2024-04-24 17:18:21 +0700

    add user service

---
+++ UserService/Database/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserService.Database
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Model.User> Users { get; set; }
+    }
+}
